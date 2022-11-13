25f80ae377f85d68ec987ef3d60e7e2e960d18be
Rom Burla <romwebdev@gmail.com>
2022-11-13 14:24:48 +0200

    added insert with loop

--- SQL homework 3/app.js
+++ SQL homework 3/app.js
@@ -12,12 +12,51 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// let gameArr = [
+//   {
+//     name: "God of war",
+//     platforms: "PS4, PS5, PC",
+//     rating: 94,
+//     price: 19.99,
+//   },
+//   {
+//     name: "God of war: Ragnarok",
+//     platforms: "PS4, PS5",
+//     rating: 94,
+//     price: 69.99,
+//   },
+//   {
+//     name: "Call of duty: Modern Warfare 2",
+//     platforms: "PS4, PS5, Xbox One, Xbox Seiries, PC",
+//     rating: 87,
+//     price: 69.99,
+//   },
+// ];
+
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
   database: "sqlhw3",
 });
+
+// let insertGames = `INSERT INTO games(\`name\`, \`platforms\`, \`metacritic_rating\`, \`price\`) VALUES`;
+// let msg = "";
+
+// gameArr.forEach((game, index) => {
+//   let sign = index === gameArr.length - 1 ? ";" : ",";
+//   msg += `("${game.name}","${game.platforms}",${game.rating},${game.price})${sign}`;
+// });
+
+// let builtQuery = insertGames + msg;
+
+// let insertGame = () => {
+//   connection.query(builtQuery, (err, result) => {
+//     if (err) console.log(err);
+//     console.log(result);
+//   });
+// };
+// insertGame();
 
 app.post("/addGame", (req, res) => {
   let { Game, Platforms, Rating, Price } = req.body;
